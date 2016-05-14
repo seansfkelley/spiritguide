9db89777524855d72faf8ea4fff926d3409a3e1b
Sean Kelley <seansfkelley@gmail.com>
2016-05-13 21:55:37 -0700

    Disallow non-development configs for now until I can figure this out and/or until I have an actual production deployment.

--- js/db/config/index.js
+++ js/db/config/index.js
@@ -2,15 +2,17 @@ import log from 'loglevel';
 
 let config;
 switch (process.env.NODE_ENV) {
-  case 'production':
-    log.info('loading config from config-production.json');
-    config = require('./config-production.json');
-    break;
+  // case 'production':
+  //   log.info('loading config from config-production.json');
+  //   config = require('./config-production.json');
+  //   break;
 
-  case 'staging':
-    log.info('loading config from config-staging.json');
-    config = require('./config-staging.json');
-    break;
+  // case 'staging':
+  //   log.info('loading config from config-staging.json');
+  //   config = require('./config-staging.json');
+  //   break;
+
+  // Currently, other settings are not supported. I'll figure it out when I need to.
 
   default:
     log.info('loading config from config-development.json');
