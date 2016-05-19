df989041e7b1fdc83078a9792bef34b237d049a2
Sean Kelley <seansfkelley@gmail.com>
2016-05-19 09:55:29 -0700

    Rename reducer files to disambiguate them from other files with the same prefix (actions, selectors).

--- js/store/reducers/index.js
+++ js/store/reducers/index.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import update from 'immutability-helper';
 
-import app from './app';
-import filters from './filters';
-import recipes from './recipes';
-import ingredients from './ingredients';
+import app from './appReducer';
+import filters from './filtersReducer';
+import recipes from './recipesReducer';
+import ingredients from './ingredientsReducer';
 
 update.extend('$without', (item, array) => _.without(array, item));
 update.extend('$omit', (key, object) => _.omit(object, key));
